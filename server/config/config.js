3814139370af387322bbf9f1e8c99bb0c7da21d1
Nicolas Nitri <nitri.nicolas@gmail.com>
2020-04-18 22:07:22 -0300

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -15,10 +15,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // ========================================
 let urlDB;
 
-// if (process.env.NODE_ENV === 'dev') {
-//     urlDB = 'mongodb://localhost:27017/cafe';
-// } else {
-urlDB = 'mongodb+srv://cafe-user:vera3440@cluster0-bxlvv.mongodb.net/test?retryWrites=true&w=majority';
-// }
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = process.env.MONGO_URI;
+}
 
 process.env.URLDB = urlDB;
